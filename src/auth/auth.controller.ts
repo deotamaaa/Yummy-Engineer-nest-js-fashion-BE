38b7c3cf02baa47257c485280ec9adecef274752
Deotama <deotama11@gmail.com>
2022-04-21 11:18:00 +0700

    Re-Init Repo

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
+
+@Controller()
+export class AuthController {
+
+  constructor(
+    private userService: UserService
+  ) { }
+
+  @Post('register')
+  async register(@Body() body) {
+    const hashed = await bcrypt.hash(body.password, 12);
+    return this.userService.create({
+      firstName: body.firstName,
+      lastName: body.lastName,
+      email: body.email,
+      password: hashed,
+    })
+  }
+}
